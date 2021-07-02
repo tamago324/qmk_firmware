f0e142701ceea8dc5d9521173347c2e001732816
tamago324 <tamago_pad@yahoo.co.jp>
2021-07-03 00:32:19 +0900

    自作キーボード入門2 1keyキーボード できた！
    * keyboards/tbook6/config.h
        * キー配列を 1x1にして、使用するピンを指定
    * keyboards/tbook6/keymaps/default/keymap.c
        * LAYOUTマクロを使って、対応するキーマップを定義
    * keyboards/tbook6/tbook6.h
        * LAYOUTマクロを定義
        * 前半は物理的なキーボードの配列
        * 後半はマトリックス？

--- keyboards/tbook6/keymaps/default/keymap.c
+++ keyboards/tbook6/keymaps/default/keymap.c
@@ -17,8 +17,7 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
-    _FN
+    _BASE
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -30,12 +29,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
-    ),
-    [_FN] = LAYOUT(
-        QMKBEST, QMKURL,  _______,
-            RESET,    XXXXXXX
+        KC_A
     )
 };
 
