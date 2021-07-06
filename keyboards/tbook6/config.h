85363563711dfc1a530279f13b93bc38aefef521
tamago324 <tamago_pad@yahoo.co.jp>
2021-07-06 22:36:57 +0900

    自作キーボード入門2 分割キーボード シリアル通信

--- keyboards/tbook6/config.h
+++ keyboards/tbook6/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      tbook6
 
 /* key matrix size */
-#define MATRIX_ROWS 3
+#define MATRIX_ROWS 4
 #define MATRIX_COLS 2
 
 /*
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { B2, B6, B3 }
+#define MATRIX_ROW_PINS { B2, B6 }
 #define MATRIX_COL_PINS { B4, B5 }
 #define UNUSED_PINS
 
@@ -51,6 +51,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
 #define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+#define SPLIT_HAND_PIN D2
 
 //#define LED_NUM_LOCK_PIN B0
 //#define LED_CAPS_LOCK_PIN B1
@@ -63,16 +64,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define BACKLIGHT_BREATHING
 
 // LEDに使用するピンを指定
-#define RGB_DI_PIN D3
-#ifdef RGB_DI_PIN
-#    define RGBLED_NUM 6
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+// #define RGB_DI_PIN D3
+// #ifdef RGB_DI_PIN
+// #    define RGBLED_NUM 6
+// #    define RGBLIGHT_HUE_STEP 8
+// #    define RGBLIGHT_SAT_STEP 8
+// #    define RGBLIGHT_VAL_STEP 8
+// #    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+// #    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 /*== all animations enable ==*/
-#    define RGBLIGHT_ANIMATIONS
+// #    define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
 //#    define RGBLIGHT_EFFECT_BREATHING
 //#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
@@ -89,7 +90,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*==== use exp() and sin() ====*/
 //#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
 //#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-#endif
+// #endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
--- keyboards/tbook6/keymaps/default/keymap.c
+++ keyboards/tbook6/keymaps/default/keymap.c
@@ -29,9 +29,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-      KC_A, KC_B, \
-      KC_C, KC_D, \
-      KC_E, KC_F \
+      KC_1, KC_2, KC_3, KC_4, \
+      KC_A, KC_B, KC_C, KC_D \
     )
 };
 
@@ -55,8 +54,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
     return true;
-}
-
-void oled_task_user(void) {
-    oled_write_P(PSTR("Hello World!"), false);
 }
