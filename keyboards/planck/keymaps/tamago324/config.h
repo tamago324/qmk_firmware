89ef51f7678e863cc965fb46eefe3578a65ce12b
tamago324 <tamago_pad@yahoo.co.jp>
2021-09-29 00:11:03 +0900

    Planck のキーマップを変更

--- keyboards/planck/keymaps/tamago324/config.h
+++ keyboards/planck/keymaps/tamago324/config.h
@@ -34,9 +34,28 @@
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
 
-#define PERMISSIVE_HOLD
-#define RETRO_TAPPING
+//#define PERMISSIVE_HOLD
+//#define RETRO_TAPPING
 
 // SFT_T(KC_SPACE) としていたときに、 Space → Space長押し としたときに、 Spaceが連続で入力されてしまう問題の対処法
 // https://docs.qmk.fm/#/ja/tap_hold?id=%e3%82%bf%e3%83%83%e3%83%94%e3%83%b3%e3%82%b0%e5%bc%b7%e5%88%b6%e3%83%9b%e3%83%bc%e3%83%ab%e3%83%89
+// https://qiita.com/uodna/items/6d23581d26c1adb23377
 #define TAPPING_FORCE_HOLD
+
+// Mod Tap Key と通常キーを連続して拘束に押したときに両方のキーを Tap として扱う
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// キーごとに タップとホールドの時間を設定する
+//#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM 150
+
+
+// // AUTO_SHIFT
+
+// // 175 がデフォルト
+// #define AUTO_SHIFT_TIMEOUT 133
+// // -_, =+, [{, ]}, ;:, ‘“, ,<, .> および /? を含む特殊キーを自動シフトしない
+// #define NO_AUTO_SHIFT_SPECIAL
+// // 数字は自動シフトしない
+// #define NO_AUTO_SHIFT_NUMERIC
+
--- keyboards/planck/keymaps/tamago324/keymap.c
+++ keyboards/planck/keymaps/tamago324/keymap.c
@@ -20,34 +20,36 @@ enum layer_number {
   _QWERTY = 0,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _ADJUST,
+  _SPECIAL
 };
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST
+  ADJUST,
+  S_SPACE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_QWERTY] = LAYOUT(
-      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,        KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_LCTRL,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,        KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,        KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-       _______,  _______, KC_LGUI, KC_LALT, LOWER, SFT_T(KC_SPACE), SFT_T(KC_SPACE), RAISE, KC_RALT, _______, _______, _______ \
+      KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,        KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      KC_LCTRL,      KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,        KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+      KC_LSHIFT,     KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,        KC_M,  KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, \
+      MO(_SPECIAL), _______, KC_LGUI, KC_LALT, LOWER, SFT_T(KC_SPACE),     SFT_T(KC_SPACE),  RAISE, KC_RALT, _______, _______, _______ \
     ),
     [_LOWER] = LAYOUT(
       KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-      _______, C(KC_A), C(KC_S), C(KC_D), C(KC_F), C(KC_G),       KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,  \
-      _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_B),       KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD, \
+      _______, C(KC_A), C(KC_S), C(KC_D), C(KC_F), C(KC_G),       KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,  \
+      _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_B),       KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, XXXXXXX, \
       _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______ \
     ),
     [_RAISE] = LAYOUT(
-      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,     KC_7,     KC_8,     KC_9,    KC_0,     XXXXXXX, \
-      _______, S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G),       KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT, XXXXXXX,  XXXXXXX, \
-      _______, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B),       KC_HOME,  KC_PGUP,  KC_PGDN,  KC_END,  XXXXXXX,  XXXXXXX, \
+      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,     KC_7,     KC_8,     KC_9,    KC_0,    XXXXXXX, \
+      _______, KC_QUOT, XXXXXXX, XXXXXXX, S(KC_QUOT), KC_GRV,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT, XXXXXXX,  XXXXXXX, \
+      _______, XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, XXXXXXX,       KC_HOME,  KC_PGUP,  KC_PGDN,  KC_END,  XXXXXXX, XXXXXXX, \
       _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______ \
     ),
     [_ADJUST] = LAYOUT(
@@ -55,6 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       C(KC_PGUP),  XXXXXXX,  XXXXXXX,  C(KC_PGDN), XXXXXXX,  XXXXXXX, \
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, \
       _______, _______,    _______,    _______,    _______,    _______,       _______, _______, _______, _______, _______, _______ \
+    ),
+    [_SPECIAL] = LAYOUT(
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ASRP, XXXXXXX, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_ASDN, KC_ASUP, XXXXXXX, XXXXXXX, XXXXXXX, \
+      KC_ASTG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET \
     )
 };
 
@@ -84,10 +92,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
 
         if (lower_pressed) {
-          register_code(KC_MHEN);
-          unregister_code(KC_MHEN);
+            tap_code(KC_MHEN);
+            lower_pressed = false;
         }
-        lower_pressed = false;
       }
       return false;
       break;
@@ -103,10 +110,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
 
         if (raise_pressed) {
-          register_code(KC_HENK);
-          unregister_code(KC_HENK);
+            // https://docs.qmk.fm/#/ja/feature_macros?id=register_codeltkcgt
+            // キーダウンイベント
+            tap_code(KC_HENK);
+            raise_pressed = false;
         }
-        raise_pressed = false;
       }
 
       return false;
@@ -121,13 +129,54 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
 
+    // case S_SPACE:
+    //     if (record->event.pressed) {
+    //         // 押した
+    //         sspace_pressed = true;
+    //         space_after_inputed = false;
+    //     } else {
+    //         // 離した
+    //         if (!space_after_inputed) {
+    //             // もし、なにも入力されていなければ、スペースを送信
+    //             tap_code(KC_SPACE);
+    //         }
+    //         sspace_pressed = false;
+    //     }
+
+    //     // QMK としてはデフォルトのキーコードは送信しない
+    //     return false;
+    //     break;
+
     default:
       if (record->event.pressed) {
+          // LOWER でもRAISEでもない文字だった場合、戻しておく
         lower_pressed = false;
         raise_pressed = false;
       }
+
+        // // もし、sspace が押しっぱなしだったら、Shiftを押した状態としてキーを送信する
+        // if (sspace_pressed) {
+        //     if (record->event.pressed) {
+        //         // Shiftを押した状態でキーコードを送信する
+        //         register_code16(S(keycode));
+        //         unregister_code16(S(keycode));
+        //         space_after_inputed = true;
+        //     }
+        //     return false;
+        // }
       break;
   }
 
   return true;
 }
+
+// //キーごとに TAPPING_TERM を設定する
+// uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//         case SFT_T(KC_SPACE):
+//             // 短くする
+//             return TAPPING_TERM - 100;
+//         default:
+//             return TAPPING_TERM;
+//     }
+// }
