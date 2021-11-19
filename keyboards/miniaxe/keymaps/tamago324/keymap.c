5814c906de11c13b7b75a533fb243336b60a2978
tamago324 <tamago_pad@yahoo.co.jp>
2021-11-19 22:50:11 +0900

    miniaxe 修正

--- keyboards/miniaxe/keymaps/tamago324/keymap.c
+++ keyboards/miniaxe/keymaps/tamago324/keymap.c
@@ -43,17 +43,17 @@ enum combo_events {
 
 // コンボ
 // DSでEscape
-const uint16_t PROGMEM comb_keys_Escape[] = {KC_D, KC_S, COMBO_END}; 
-const uint16_t PROGMEM comb_keys_semicoron[] = {KC_K, KC_L, COMBO_END}; 
-const uint16_t PROGMEM comb_keys_ctrl_space[] = {KC_X, KC_C, COMBO_END}; 
-const uint16_t PROGMEM comb_keys_slash[] = {KC_J, KC_K, COMBO_END}; 
+const uint16_t PROGMEM comb_keys_Escape[] = {KC_D, KC_S, COMBO_END};
+const uint16_t PROGMEM comb_keys_semicoron[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM comb_keys_ctrl_space[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM comb_keys_slash[] = {KC_J, KC_K, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   [CMB_ESCAPE] = COMBO(comb_keys_Escape, KC_ESC),
   [CMB_SEMICORON] = COMBO(comb_keys_semicoron, JU_SCLN),
   [CMB_CTRL_SPACE] = COMBO(comb_keys_ctrl_space, C(KC_SPACE)),
   [CMB_SLASH] = COMBO(comb_keys_slash, KC_SLSH),
-}; 
+};
 
 /* void process_combo_event(uint16_t combo_index, bool pressed) { */
 /*   switch(combo_index) { */
@@ -108,19 +108,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_split_3x5_3( \
       KC_EXLM, JP_AT,   KC_HASH, KC_DLR,  KC_PERC,              JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN, JP_RPRN, \
       KC_LCTRL, XXXXXXX, S(KC_COMM), S(KC_DOT), JP_TILD,         JP_MINS, JP_EQL,  JP_LBRC, JP_RBRC, JP_COLN, \
-      XXXXXXX, XXXXXXX, XXXXXXX, JP_BSLS, JP_PIPE,              JP_UNDS, JP_PLUS, JP_LCBR, JP_RCBR, S(KC_SLSH),\
-                        _______, _______, _______,              KC_LCTRL, _______, _______ \
+      SWIN(KC_S), XXXXXXX, XXXXXXX, JP_BSLS, JP_PIPE,              JP_UNDS, JP_PLUS, JP_LCBR, JP_RCBR, S(KC_SLSH),\
+                        _______, _______, _______,              KC_SLSH, _______, _______ \
   ),
   [_RAISE] = LAYOUT_split_3x5_3( \
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                 KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     \
-      _______, JU_QUOT, JU_QUOT, JU_QUOT, JP_GRV,               KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT, JP_COLN, \
-      _______, XXXXXXX, XXXXXXX, KC_SLSH, KC_SLSH,              KC_HOME,  KC_PGUP,  KC_PGDN, KC_END,  KC_SLSH,   \
+      KC_LCTRL, JU_QUOT, JU_QUOT, JU_QUOT, JP_GRV,               KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT, JP_COLN, \
+      _______, XXXXXXX, KC_SLSH, KC_SLSH, KC_SLSH,              KC_HOME,  KC_PGUP,  KC_PGDN, KC_END,  KC_SLSH,   \
                         _______, _______, _______,              _______, _______, _______  \
   ),
   [_ADJUST] =  LAYOUT_split_3x5_3( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,               KC_F6,    KC_F7,    KC_F8,   KC_F9,   KC_F10,    \
-      KC_F11, JP_DQUO,   JP_DQUO, JP_DQUO, JP_GRV,             C(KC_PGUP), XXXXXXX, XXXXXXX, C(KC_PGDN), KC_F12,\
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, \
+      KC_LCTRL, JP_DQUO,   JP_DQUO, JP_DQUO, JP_GRV,             C(KC_PGUP), XXXXXXX, XXXXXXX, C(KC_PGDN), KC_F12,\
+      KC_F11, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, \
                         _______, _______, _______,             _______, _______, RESET                           \
   )
 };
@@ -249,14 +249,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case TD(TD_ALT_GUI):
-            return TAPPING_TERM + 700;
+            return TAPPING_TERM + 500;
             break;
         /* case LCTL_T(KC_A): */
         /*     return TAPPING_TERM + 5; */
         /*     break; */
         case LSFT_T(KC_Z):
-            // ze ってうったときだけ、おおくしたいな...
             return TAPPING_TERM + 30;
+            break;
+        case SFT_T(KC_ENT):
+            return TAPPING_TERM - 20;
             break;
         default:
             return TAPPING_TERM;
