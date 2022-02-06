49960e213fffece1f5abb3339b34eb61a11a8bca
tamago324 <tamago_pad@yahoo.co.jp>
2022-02-06 19:29:06 +0900

    update

--- keyboards/bastardkb/skeletyl/keymaps/tamago324/config.h
+++ keyboards/bastardkb/skeletyl/keymaps/tamago324/config.h
@@ -28,3 +28,13 @@
 
 // キーごとに設定する
 #define COMBO_TERM_PER_COMBO
+
+
+
+// サイズを小さくするためのもの https://zenn.dev/koron/articles/98324ab760e83a
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+#undef NO_ACTION_ONESHOT // ワンショットキーの無効化
+
+#define LAYER_STATE_8BIT    // レイヤーの上限を8にする
--- keyboards/bastardkb/skeletyl/keymaps/tamago324/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/tamago324/keymap.c
@@ -1,4 +1,5 @@
-/* Copyright 2018 ENDO Katsuhiro <ka2hiro@curlybracket.co.jp>
+/* 
+ * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include QMK_KEYBOARD_H
 #include "keymap_jp.h"
 
@@ -22,8 +24,8 @@ enum layer_names {
   _LOWER,
   _RAISE,
   _ADJUST,
-  _OPENING_BRACKET,
-  _CLOSING_BRACKET,
+  // _OPENING_BRACKET,
+  // _CLOSING_BRACKET,
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -33,7 +35,9 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   JU_SCLN,
-  JU_QUOT
+  JU_QUOT,
+  ALT_TAB,
+  ALT_SHIFT_TAB
 };
 
 enum combo_events {
@@ -42,8 +46,11 @@ enum combo_events {
   CMB_CTRL_SPACE,
   // CMB_MINUS1,
   // CMB_MINUS2,
-  CMB_OPENING_BRACKET_LAYER,
-  CMB_CLOSING_BRACKET_LAYER,
+  // CMB_OPENING_BRACKET_LAYER,
+  // CMB_CLOSING_BRACKET_LAYER,
+  CMB_BACKSPACE,
+  CMB_WIN_ENTER,
+  CMB_WIN,
 };
 
 // コンボ
@@ -54,6 +61,10 @@ const uint16_t PROGMEM comb_keys_ctrl_space[] = {KC_X, KC_C, COMBO_END};
 // const uint16_t PROGMEM comb_keys_dash2[] = {JP_EQL, JP_LBRC, COMBO_END};
 const uint16_t PROGMEM comb_keys_opening_bracket_layer[] = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM comb_keys_closing_bracket_layer[] = {KC_C, KC_V, COMBO_END};
+// const uint16_t PROGMEM comb_keys_back_space[] = {KC_COMM, KC_DOT, COMBO_END};
+// Lower の SD
+const uint16_t PROGMEM comb_keys_win_enter[] = {JP_PIPE, S(KC_COMM), COMBO_END};
+const uint16_t PROGMEM comb_keys_win[] = {JP_AT, KC_HASH, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   [CMB_ESCAPE] = COMBO(comb_keys_Escape, KC_ESC),
@@ -61,8 +72,11 @@ combo_t key_combos[COMBO_COUNT] = {
   [CMB_CTRL_SPACE] = COMBO(comb_keys_ctrl_space, C(KC_SPACE)),
   // [CMB_MINUS1] = COMBO(comb_keys_dash, JP_MINS),
   // [CMB_MINUS2] = COMBO(comb_keys_dash2, JP_MINS),
-  [CMB_OPENING_BRACKET_LAYER] = COMBO(comb_keys_opening_bracket_layer, MO(_OPENING_BRACKET)),
-  [CMB_CLOSING_BRACKET_LAYER] = COMBO(comb_keys_closing_bracket_layer, MO(_CLOSING_BRACKET)),
+  // [CMB_OPENING_BRACKET_LAYER] = COMBO(comb_keys_opening_bracket_layer, MO(_OPENING_BRACKET)),
+  // [CMB_CLOSING_BRACKET_LAYER] = COMBO(comb_keys_closing_bracket_layer, MO(_CLOSING_BRACKET)),
+  // [CMB_BACKSPACE] = COMBO(comb_keys_back_space, KC_BSPC),
+  [CMB_WIN_ENTER] = COMBO(comb_keys_win_enter, LWIN(KC_ENT)),
+  [CMB_WIN] = COMBO(comb_keys_win, KC_LGUI),
 };
 
 
@@ -90,14 +104,20 @@ const key_override_t shift_bspc_delete = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, 
 const key_override_t syounari = ko_make_basic(MOD_MASK_CTRL, KC_COMM, S(KC_COMM));
 // >
 const key_override_t dainari = ko_make_basic(MOD_MASK_CTRL, KC_DOT, S(KC_DOT));
-// Alt+Shift+Tab
-const key_override_t alt_shift_tab = ko_make_basic(MOD_MASK_ALT, KC_W, S(LALT(KC_TAB)));
+// // // Alt+Shift+Tab
+// // const key_override_t alt_shift_tab = ko_make_basic(MOD_MASK_ALT, KC_W, S(LALT(KC_TAB)));
+// // Alt+Shift+Tab
+// const key_override_t alt_shift_tab2 = ko_make_basic(MOD_MASK_ALT, KC_B, S(LALT(KC_TAB)));
+// // Alt+Tab
+// const key_override_t alt_tab = ko_make_basic(MOD_MASK_ALT, KC_G, LALT(KC_TAB));
 
 const key_override_t **key_overrides = (const key_override_t *[]) {
   &shift_bspc_delete,
   &syounari,
   &dainari,
-  &alt_shift_tab,
+  // &alt_shift_tab,
+  // &alt_shift_tab2,
+  // &alt_tab,
   NULL
 };
 
@@ -108,52 +128,41 @@ enum {
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   // 1回のタップで ALT、2回のタップで GUI
-  [TD_ALT_GUI] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_LGUI)
+  [TD_ALT_GUI] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_LGUI),
 };
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x5_3( \
          KC_TAB,  KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y, KC_U,  KC_I,    KC_O,    KC_BSPC, \
          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H, KC_J,  KC_K,    KC_L,    KC_P, \
-  LSFT_T(KC_Z),   KC_X,    KC_C,    KC_V,    KC_B,                          KC_N, KC_M,  KC_COMM, KC_DOT,  KC_Q, \
-                    TD(TD_ALT_GUI),  LOWER,  LCTL_T(KC_SPACE),     SFT_T(KC_ENT), RAISE, KC_LALT  \
+  LSFT_T(KC_Z),   KC_X,    KC_C,    ALT_T(KC_V),    KC_B,                   KC_N, KC_M,  KC_COMM, KC_DOT,  ALT_T(KC_Q), \
+                    LOWER,  LCTL_T(KC_SPACE), XXXXXXX,                XXXXXXX,  SFT_T(KC_ENT), RAISE \
   ),
   [_LOWER] = LAYOUT_split_3x5_3( \
-      KC_EXLM, JP_AT,   KC_HASH, KC_DLR,  KC_PERC,              JP_CIRC, JP_AMPR, JP_ASTR, JP_LPRN, JP_RPRN, \
+      KC_EXLM, JP_AT,   KC_HASH, KC_DLR,  KC_PERC,              JP_ASTR, JP_AMPR, JP_LPRN, JP_RPRN, JP_CIRC,  \
       KC_LCTRL, JP_PIPE, S(KC_COMM), S(KC_DOT), JP_TILD,        KC_SLSH, JP_MINS,  JP_LBRC, JP_RBRC, JP_COLN, \
-      SWIN(KC_S), KC_DELETE, XXXXXXX, C(KC_Z), KC_ENT,          JP_UNDS, JP_EQL, JP_LCBR, JP_RCBR, S(KC_SLSH),\
-                        _______, _______, _______,              _______, _______, _______ \
+      C(KC_Z), SWIN(KC_S), ALT_SHIFT_TAB, ALT_TAB, KC_ENT,          JP_UNDS, JP_EQL, JP_LCBR, JP_RCBR, S(KC_SLSH),\
+                        _______, _______, _______,              _______, LALT(KC_ENT), _______ \
   ),
   [_RAISE] = LAYOUT_split_3x5_3( \
-      XXXXXXX,  KC_4, KC_5, KC_6, XXXXXXX,               XXXXXXX,  XXXXXXX, JU_QUOT, JU_QUOT,  XXXXXXX,     \
-      KC_LCTRL, KC_1, KC_2, KC_3, JP_BSLS,              KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT, JP_COLN, \
-      KC_LSFT,  KC_7, KC_8, KC_9, JP_PLUS,             KC_HOME,  KC_PGUP,  KC_PGDN, KC_END,  KC_DELETE,   \
-               _______, _______, CTL_T(KC_0),              _______, _______, _______  \
+      XXXXXXX,  KC_4, KC_5, KC_6, XXXXXXX,               XXXXXXX,  XXXXXXX, JU_QUOT, JU_QUOT, XXXXXXX,     \
+      XXXXXXX, KC_1, KC_2, KC_3, JP_BSLS,              KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT, XXXXXXX, \
+      KC_LSFT,  KC_7, KC_8, KC_9, XXXXXXX,             KC_HOME,  KC_PGUP,  KC_PGDN, KC_END,  KC_DELETE,   \
+                        _______, CTL_T(KC_0), _______,              _______, _______, _______  \
   ),
   [_ADJUST] =  LAYOUT_split_3x5_3( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,               KC_F6,    KC_F7,    KC_F8,   KC_F9,   KC_F10,    \
-      KC_LCTRL, JP_DQUO,   JP_DQUO, XXXXXXX, JP_GRV,             C(KC_PGUP), XXXXXXX, XXXXXXX, C(KC_PGDN), KC_F12,\
-      KC_F11, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, \
+      KC_LCTRL, JP_DQUO, JP_DQUO, JP_DQUO, XXXXXXX,             C(KC_PGUP), XXXXXXX, XXXXXXX, C(KC_PGDN), KC_F12,\
+      KC_F11, XXXXXXX, JP_GRV, JP_GRV, XXXXXXX,             XXXXXXX,  JP_PLUS,  XXXXXXX, XXXXXXX, XXXXXXX, \
                         _______, _______, _______,             _______, _______, XXXXXXX                           \
   ),
-  [_OPENING_BRACKET] =  LAYOUT_split_3x5_3( \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             KC_Y, KC_U,  KC_I,    KC_O,    XXXXXXX, \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,               KC_H, KC_J,  KC_K,    KC_L,    KC_P, \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             KC_N, KC_M,  KC_COMM, KC_DOT,  KC_Q, \
-                        XXXXXXX, _______, _______,             _______, _______, XXXXXXX                           \
-  ),
-  [_CLOSING_BRACKET] =  LAYOUT_split_3x5_3( \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             KC_Y, KC_U,  KC_I,    KC_O,    XXXXXXX, \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,               KC_H, KC_J,  KC_K,    KC_L,    KC_P, \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             KC_N, KC_M,  KC_COMM, KC_DOT,  KC_Q, \
-                        XXXXXXX, _______, _______,             _______, _______, XXXXXXX \
-  )
 };
 
 static bool lower_pressed = false;
 static bool raise_pressed = false;
 static bool zshift_pressed = false;
+static bool alt_tab_pressed = false;
+static bool alt_shift_tab_pressed = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
@@ -176,6 +185,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        if (alt_tab_pressed || alt_shift_tab_pressed) {
+          // もし、Lower を離したときに、ALT が押しっぱになっていたら、離す
+          unregister_code(KC_LALT);
+          alt_tab_pressed = false;
+          alt_shift_tab_pressed = false;
+          // また、無変換は送信しない
+          return false;
+        }
 
         if (lower_pressed) {
             tap_code(KC_MHEN);
@@ -272,9 +290,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
 
     case KC_E:
-    case KC_O:
-    case KC_U:
-    case KC_I:
+    // case KC_O:
+    // case KC_U:
+    // case KC_I:
       // z と同時に押したら、zX にする
       if (record->event.pressed) {
           if (zshift_pressed) {
@@ -294,6 +312,46 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 
+    case ALT_TAB:
+      if (record->event.pressed) {
+        // 初めて押したら、Altを押し続ける
+        // また、TAB も送信する
+        if (!alt_tab_pressed || !alt_shift_tab_pressed) {
+          lower_pressed = false;
+          raise_pressed = false;
+          zshift_pressed = false;
+        
+          alt_tab_pressed = true;
+
+          // ALT を押し続ける
+          register_code(KC_LALT);
+        }
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      break;
+
+    case ALT_SHIFT_TAB:
+      if (record->event.pressed) {
+        // 初めて押したら、Altを押し続ける
+        // また、TAB も送信する
+        if (!alt_tab_pressed || !alt_shift_tab_pressed) {
+          lower_pressed = false;
+          raise_pressed = false;
+          zshift_pressed = false;
+        
+          alt_shift_tab_pressed = true;
+
+          // ALT を押し続ける
+          register_code(KC_LALT);
+        }
+        register_code16(S(KC_TAB));
+      } else {
+        unregister_code16(S(KC_TAB));
+      }
+      break;
+
     default:
       if (record->event.pressed) {
           // LOWER でもRAISEでもない文字だった場合、戻しておく
@@ -301,13 +359,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         raise_pressed = false;
         zshift_pressed = false;
 
-        if (IS_LAYER_ON(_OPENING_BRACKET)) {
-          // [
-          tap_code(JP_LBRC);
-        } else if (IS_LAYER_ON(_CLOSING_BRACKET)) {
-          // ]
-          tap_code(JP_RBRC);
-        }
+        // if (IS_LAYER_ON(_OPENING_BRACKET)) {
+        //   // [
+        //   tap_code(JP_LBRC);
+        // } else if (IS_LAYER_ON(_CLOSING_BRACKET)) {
+        //   // ]
+        //   tap_code(JP_RBRC);
+        // }
       }
 
       break;
@@ -330,6 +388,9 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             break;
         case SFT_T(KC_ENT):
             return TAPPING_TERM - 20;
+            break;
+        case ALT_T(KC_V):
+            return TAPPING_TERM + 30;
             break;
         // case LCTL_T(KC_SPACE):
         //     return TAPPING_TERM + 20;
