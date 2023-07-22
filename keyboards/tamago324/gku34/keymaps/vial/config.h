959181d30c41a848daefa1779c149eb462909f49
tamago324 <tamago_pad@yahoo.co.jp>
2023-07-23 01:19:46 +0900

    add vial firmware for gku34

---
+++ keyboards/tamago324/gku34/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0xCF, 0xA6, 0xCF, 0x1B, 0x18, 0x4A, 0xAF}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
