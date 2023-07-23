1b8840f7d3f9c0358f59d30da0d69f95ed62c4c7
tamago324 <tamago_pad@yahoo.co.jp>
2023-07-23 14:55:42 +0900

    update vial firmware

--- keyboards/tamago324/gku34/keymaps/vial/config.h
+++ keyboards/tamago324/gku34/keymaps/vial/config.h
@@ -5,3 +5,8 @@
 #define VIAL_KEYBOARD_UID {0xFC, 0xCF, 0xA6, 0xCF, 0x1B, 0x18, 0x4A, 0xAF}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
+
+#define LAYER_STATE_8BIT    // レイヤーの上限を8にする
+
+// Shift が抜ける問題への対処法の1つらしい
+#define USB_POLLING_INTERVAL_MS 10
