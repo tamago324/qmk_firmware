1d307fe6744592f58d3e5b6923e077e0bd897707
tamago324 <tamago_pad@yahoo.co.jp>
2024-04-03 22:27:12 +0900

    Update config.h

--- keyboards/tamago324/gku34/keymaps/vial/config.h
+++ keyboards/tamago324/gku34/keymaps/vial/config.h
@@ -4,7 +4,7 @@
 
 #define VIAL_KEYBOARD_UID {0xFC, 0xCF, 0xA6, 0xCF, 0x1B, 0x18, 0x4A, 0xAF}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
 // レイヤーの上限を8にする
 #define LAYER_STATE_8BIT
